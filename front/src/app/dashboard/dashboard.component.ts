6423d13245234db278e9c405cfcbf4a61679c982
Seraphin <adulsonseraphin.daluze@edu.esiee.fr>
2024-02-14 20:52:55 +0100

    material 1.0.0

--- front/src/app/dashboard/dashboard.component.ts
+++ front/src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,7 +6,19 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  animations: [
+    // Define your animations here
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition(':enter, :leave', [
+        animate(1000)
+      ])
+    ])
+  ]
+
 })
 export class DashboardComponent {
 
--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { AuthServiceService } from '../auth-service.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatCardModule,} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,11 +24,45 @@ export class LoginComponent {
    credentials = { username: "", password: "" };
    username: string = '';
    password: string = '';
-
+   loginForm: FormGroup;
+   isLoading: boolean = false;
   
-  constructor(private authService: AuthServiceService, private router: Router) {}
+  constructor(private authService: AuthServiceService, private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   login() {
+    if (this.loginForm.valid) {
+      console.log("===== LOGIN ======"+JSON.stringify(this.loginForm.value));
+      this.credentials = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password
+      };
+    this.authService.login(this.credentials).subscribe(res => {
+      // Redirect to the desired route after successful login
+      if(res){
+        console.log("SUCCESS !"+JSON.stringify(res.users));
+        this.router.navigate(['/dashboard']);
+      }else{
+        console.log("ERR1!");
+
+      }
+    }, error => {
+      // Handle login error
+      console.log("ERR!");
+    });
+      this.isLoading = true;
+      // Simulate login process with a delay
+      setTimeout(() => {
+        // Perform actual login logic here
+        this.isLoading = false;
+      }, 2000);
+    }
+  }
+  /*login() {
     console.log("===== LOGIN ======");
     this.authService.login(this.credentials).subscribe(res => {
       // Redirect to the desired route after successful login
@@ -50,5 +92,5 @@ export class LoginComponent {
           console.error('Login failed:', error);
         }
       );
-  }
+  }*/
 }
